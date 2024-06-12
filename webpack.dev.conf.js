198185c822b58ae1838335fdc1d6366fe02ca89e
liujun <1234567890>
2024-06-12 14:13:40 +0800

    增加js 压缩

--- webpack.dev.conf.js
+++ webpack.dev.conf.js
@@ -1,6 +1,7 @@
 const merge = require('webpack-merge');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const ManifestPlugin = require('webpack-manifest-plugin');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
 const baseWebpackConfig = require('./webpack.base.conf');
 
 const webpackConfig = merge(baseWebpackConfig, {
@@ -40,6 +41,29 @@ const webpackConfig = merge(baseWebpackConfig, {
         },
         hot: true,
         host: '0.0.0.0'
+    },
+    productionSourceMap: false, // 设为false，既可以减少包大小，也可以加密源码
+    chainWebpack(config) {
+        //最小化代码
+        config.optimization.minimize(true);
+        //分割代码
+        config.optimization.splitChunks({chunks: 'all'});
+        //默认开启prefetch(预先加载模块)，提前获取用户未来可能会访问的内容 在首屏会把这十几个路由文件，都一口气下载了 所以我们要关闭这个功能模块
+        config.plugins.delete('prefetch');
+        if (isProdOrTest) {
+            // 对超过10kb的文件gzip压缩
+            config.plugin('compressionPlugin').use(
+                new CompressionWebpackPlugin({
+                    test: /\.(js|css|html)$/, // 匹配文件名
+                    filename: '[path].gz[query]',　// 压缩后的文件名
+                    algorithm: 'gzip',　// 使用gzip压缩
+                    minRatio: 1,　// 压缩率小于1才会压缩
+                    threshold: 10240,
+                    deleteOriginalAssets: false //是否删除原文件
+                })
+            );
+        };
+
     },
     module: {
         rules: [
